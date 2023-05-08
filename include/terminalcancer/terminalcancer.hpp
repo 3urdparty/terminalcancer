fbd82beab6d9c911fb5a10c4d3e56c32e878cb15
3urd Party <3urdparty@gmail.com>
2023-05-08 09:15:52 +0800

    Changed display table

--- include/terminalcancer/terminalcancer.hpp
+++ include/terminalcancer/terminalcancer.hpp
@@ -8,10 +8,14 @@
 #include <tuple>
 #include <utility>
 #include <vector>
+#include <unistd.h>
+#include <termios.h>
 using namespace std;
 
 /// @brief returns the dimensions of the current terminal
 /// @return returns a tuple containing the terminal width and height
 tuple<int, int> getTerminalDimensions();
 
+char getch(void);
+char getche(void);
 #endif
--- src/terminalcancer.cpp
+++ src/terminalcancer.cpp
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <sys/ioctl.h>
+#include <termios.h>
 #include <unistd.h>
 
 #include <tuple>
@@ -17,3 +18,39 @@ tuple<int, int> getTerminalDimensions() {
   // of rows (height)
   return {w.ws_col, w.ws_row};
 };
+
+char getch(void) {
+  char buf = 0;
+  struct termios old = {0};
+  fflush(stdout);
+  if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+  if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
+  if (read(0, &buf, 1) < 0) perror("read()");
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+  if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("tcsetattr ~ICANON");
+  printf("%c\n", buf);
+  return buf;
+}
+
+char getche(void) {
+  char buf = 0;
+  struct termios old = {0};
+  fflush(stdout);
+  if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+  if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
+  if (read(0, &buf, 1) < 0) perror("read()");
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+  if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("tcsetattr ~ICANON");
+  // printf("%c\n", buf); // uncomment this if you want to see the char
+  return buf;
+}
